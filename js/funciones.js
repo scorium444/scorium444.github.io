207a6f0b95e37ce41386ef4da90a4bddba61fbdf
scorium444 <100055096+scorium444@users.noreply.github.com>
2022-03-21 21:21:31 -0300

    Add files via upload

--- js/funciones.js
+++ js/funciones.js
@@ -1,8 +1,6 @@
-
-// AGREGAR UNA MASCOTA NUEVA
-
-function agregar(){
-
+const boton = document.getElementById("boton")
+// AGREGAR MASCOTAS
+boton.addEventListener("click", function(e) {
     function Mascotas(nombre, raza, dueño, peso) {
         this.nombre = nombre
         this.raza = raza
@@ -15,29 +13,54 @@ function agregar(){
     let capturardueño = document.getElementById("dueño").value;
     let capturarpeso = document.getElementById("peso").value;
     nuevaMascota = new Mascotas(capturadorasnombre, capturarraza, capturardueño, capturarpeso)
-    agregarMascota()
-}
-//ARRAY 
+    console.log("estas aqui");
+    paraEmpezar()
+})
+// ARRAY
 const nombresMascotas = [
-    {nombre: "Limon", raza: "Caniche", dueño: "Karen", peso: 6},
-    {nombre: "Pelusa", raza: "Angora", dueño: "Stuart", peso: 4},
-    {nombre: "Nube", raza: "Shitzu", dueño: "Marta", peso: 2},
-    {nombre: "Asesino", raza: "Rotwailler", dueño: "ElBrian", peso: 55},
-    {nombre: "Fatiga", raza: "Mestizo", dueño: "Pepe", peso: 22},
-    {nombre: "Lazzie", raza: "Collie", dueño: "John", peso: 20},
+    {nombre: "", raza: "", dueño: "", peso: ""},
+
 ]
-
-    function agregarMascota(){
-        nombresMascotas.push(nuevaMascota)
-        document.getElementById("table").innerHTML += `<tbody><td>${nuevaMascota.nombre}</td><td>${nuevaMascota.raza}</td><td>${nuevaMascota.dueño}</td><td>${nuevaMascota.peso}</td></tbody>`
+// FUNCIONES
+function agregarMascota(){
+    almacenados.push(nuevaMascota)
+    document.getElementById("table").innerHTML += `<tbody><td>${nuevaMascota.nombre}</td><td>${nuevaMascota.raza}</td><td>${nuevaMascota.dueño}</td><td>${nuevaMascota.peso}</td></tbody>`
+    const nuevo = JSON.stringify(almacenados)
+    localStorage.setItem("baul", nuevo)
+}
+function paraEmpezar() {
+    if(localStorage.getItem("baul") === null){
+        const inicioStorage = JSON.stringify(nombresMascotas)
+        localStorage.setItem("baul", inicioStorage)
     }
-//BUSCAR MASCOTA EN EL ARRAY    
-function buscar() {
-    let porNombre = document.getElementById("buscarnombre").value;
-    buscarpeso = nombresMascotas.find(e => e.nombre == porNombre)
-    document.getElementById("table2").innerHTML += `<tbody><td>${buscarpeso.nombre}</td><td>${buscarpeso.raza}</td><td>${buscarpeso.dueño}</td><td>${buscarpeso.peso}</td></tbody>`
-
+    else{
+        agregarMascota()
+    }    
     
 }
-const solonombres = nombresMascotas.map((el) => el.nombre)
-console.log(`Buscar con estos nombres: ${solonombres}`);
+//BUSCAR MASCOTA EN EL ARRAY  
+const boton2 = document.getElementById("boton2")  
+boton2.addEventListener("click",function(e){
+    e.preventDefault
+    let porNombre = document.getElementById("buscarnombre").value;
+    porNombre.toLowerCase()
+    buscarpeso = almacenados.filter(e => e.nombre == porNombre)
+    console.log(buscarpeso);
+    buscarpeso.forEach(element => {
+        document.getElementById("table2").innerHTML += `<tbody><td>${element.nombre}</td><td>${element.raza}</td><td>${element.dueño}</td><td>${element.peso}</td></tbody>`
+    });
+    
+
+    
+})
+
+//TABLA
+const almacenados = JSON.parse(localStorage.getItem("baul"))
+// si no tiene nombre no aparece
+almacenados.forEach(element => {
+    if(element.nombre != ""){
+    document.getElementById("table").innerHTML += `<tbody><td>${element.nombre}</td><td>${element.raza}</td><td>${element.dueño}</td><td>${element.peso}</td></tbody>`
+    }    
+    })
+
+console.table(almacenados)
